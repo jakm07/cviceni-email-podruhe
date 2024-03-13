49b9376677a68af6fff8bf62b5227fb5d65a2d30
Michaela Jakubikova <jakubi.mich@gmail.com>
2024-03-13 21:05:32 +0100

    nevim uz co

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const goodbye = (name) => {
   return 'S pozdravem ' + name;
 };
 
+const formalGoodbye = (name) => {
+  return 'S přáním hezkého dne ' + name
+};
+
+const rudeGoodbye = (name) => {
+  return 'Měj se ' + name
+};
+
 const fillSubject = (subject) => {
   document.querySelector('.email__subject').textContent = subject;
 };
 
-const fillBody = (body, name) => {
+const fillBody = (body, name, goodbyeFunction) => {
   const bodyElm = document.querySelector('.email__body');
   bodyElm.innerHTML += body;
   const closingElm = document.querySelector('.email__closing');
-  closingElm.textContent = goodbye(name);
+  closingElm.textContent = goodbyeFunction(name);
 };
+
+fillSubject('Pozvánka na oslavu')
+fillBody('Přijď na oslavu!', 'Patrik Veselý', rudeGoodbye)
